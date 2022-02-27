6bba0fc0bd18c23e335fc2cca3024636e7ddc3b5
phongtruong2106 <thpgamer42@gmail.com>
2022-02-27 22:05:22 +0700

    update ss2 week 3

---
+++ src/com/truonghoaiphong2004110017/tuan3/buoi2/LionTestDrive.java
@@ -0,0 +1,9 @@
+package com.truonghoaiphong2004110017.tuan3.buoi2;
+
+public class LionTestDrive {
+    public static void main(String[] args) {
+        Lion lion = new Lion();
+        lion.eat();
+        lion.makeNoise();
+    }
+}
