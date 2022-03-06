d6d511d1561ce0f74ca7ffac1036faa395b1b326
phongtruong2106 <thpgamer42@gmail.com>
2022-03-06 13:59:18 +0700

    update account bank

---
+++ src/com/truonghoaiphong2004110017/tuan3/buoi2/bai4/AccountTestDrive.java
@@ -0,0 +1,12 @@
+package com.truonghoaiphong2004110017.tuan3.buoi2.bai4;
+
+public class AccountTestDrive {
+    public static void main(String[] args) {
+         Account account = new Account(103837, "Trương Hoài Phong", 100.000);
+         account.rutTien();
+         account.napTien();
+         account.print();
+
+
+    }
+}
