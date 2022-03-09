7059ae660732a332f5bbde3db6cef19ee0eb1a63
phongtruong2106 <thpgamer42@gmail.com>
2022-03-09 15:08:26 +0700

    update

---
+++ src/com/truonghoaiphong2004110017/tuan4/buoi8/CowTestDrive.java
@@ -0,0 +1,12 @@
+package com.truonghoaiphong2004110017.tuan4.buoi8;
+
+public class CowTestDrive {
+    public static void main(String[] args) {
+        Cow cow = new Cow();
+        System.out.println(cow);
+
+        Cow cow2 = new Cow(1000, "Ha lan");
+        System.out.println(cow2);
+    }
+
+}
