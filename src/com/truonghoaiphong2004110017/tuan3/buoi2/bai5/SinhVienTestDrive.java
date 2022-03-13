8d1a17ba6b8f3906b1079b6a2d8750c3e475c7f2
phongtruong2106 <thpgamer42@gmail.com>
2022-03-13 16:02:48 +0700

    update Sinh vien

--- src/com/truonghoaiphong2004110017/tuan3/buoi2/bai5/SinhVienTestDrive.java
+++ src/com/truonghoaiphong2004110017/tuan3/buoi2/bai5/SinhVienTestDrive.java
@@ -4,20 +4,36 @@ import java.util.Scanner;
 
 public class SinhVienTestDrive {
     static Scanner sc= new Scanner(System.in);
-    static void Input(SinhVien sv){
-        System.out.println("nhập mssv: ");
-        sv.setId(sc.nextInt());
-        sc.nextLine();
-        System.out.println("nhập họ và tên: ");
-        sv.setHoTen(sc.nextLine());
-        System.out.println("nhập địa chỉ: ");
-        sv.setDiaChi(sc.nextLine());
-        do{
-            System.out.println("nhập số điện thoại(7 số) :");
-            sv.setSdt(sc.nextLine());
-        } while (sv.getSdt().length() != 7);
-    }
     public static void main(String[] args) {
-         
+        SinhVien sVien[] = null;
+        System.out.println("-nhập số lượng sinh viên; ");
+        int a = sc.nextInt();
+        sVien = new SinhVien[a];
+        for(int i = 0; i < a; i++){
+            System.out.println("nhập sinh viên thứ " + (i + 1) + " : ");
+            sVien[i] = new SinhVien();
+            sVien[i].Input();
+        }
+        //sắp xếp theo mã sinh viên
+    SinhVien sx = null;
+    for( int i = 0; i < a -1; i++){
+        for ( int j = i + 1; j < a; j++){
+            if(sVien[i].getId() > sVien[j].getId()){
+                 sx = sVien[i];
+                 sVien[i] = sVien[j];
+                 sVien[j] = sx;
+                }
+           
+             }                    
+        
+        }
+        //xuất ra màn hình]
+        System.out.println("danh sách sinh viên");
+
+        for(int i = 0; i < a; i++){
+            System.out.println(sVien[i].toString());
+        }
     }
+
+    
 }
