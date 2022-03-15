9e19de4f28edeec05fdb012dabcc1ad9a3b585c2
phongtruong2106 <thpgamer42@gmail.com>
2022-03-15 20:40:05 +0700

    update

---
+++ src/com/truonghoaiphong2004110017/tuan5/buoi1/bai2/SachThamKhao.java
@@ -0,0 +1,48 @@
+package com.truonghoaiphong2004110017.tuan5.buoi1.bai2;
+
+public class SachThamKhao extends ThuVien{
+    private double thanhTien, thue, TBC;
+
+     
+    public SachThamKhao(){
+
+    }
+
+    public SachThamKhao(double thanhTien, double thue){
+        this.thanhTien = thanhTien;
+        this.thue = thue;
+    }
+
+    public void setThanhTien(double thanhTien) {
+        this.thanhTien = thanhTien;
+    }
+
+    public double getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setThue(double thue) {
+        this.thue = thue;
+    }
+
+    public double getThue() {
+        return thue;
+    }
+
+    public void input(){
+        super.input();
+        System.out.println("input tax: ");
+        thue = sc.nextDouble();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString() + ",TAX: " + this.thue + " ,thành tiền: " + thanhTien();
+    }
+
+    public double thanhTien(){
+        return thanhTien = this.getUnitPrice() *  this.getUnitPrice() + this.thue;
+    }
+
+}
